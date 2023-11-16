13dbea33c45150734c6dfad8f034d5bb8cdd69a0
HectorContrerasD <201G0239@rcarbonifera.tecnm.mx>
2023-11-15 18:38:58 -0600

    databaseconection

---
+++ Registrodelecturas/Models/Lecturas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Registrodelecturas.Models;
+
+public partial class Lecturas
+{
+    public int Id { get; set; }
+
+    public string NombreEsp { get; set; } = null!;
+
+    public string Variable { get; set; } = null!;
+
+    public float Lectura { get; set; }
+
+    public DateTime? Hora { get; set; }
+}
---
+++ Registrodelecturas/Models/LecturasespContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Registrodelecturas.Models;
+
+public partial class LecturasespContext : DbContext
+{
+    public LecturasespContext()
+    {
+    }
+
+    public LecturasespContext(DbContextOptions<LecturasespContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Lecturas> Lecturas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;user=root;password=root;database=lecturasesp", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Lecturas>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("lecturas");
+
+            entity.Property(e => e.Hora)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp");
+            entity.Property(e => e.NombreEsp).HasMaxLength(45);
+            entity.Property(e => e.Variable).HasMaxLength(45);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
